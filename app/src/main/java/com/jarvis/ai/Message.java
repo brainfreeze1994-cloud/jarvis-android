61b0c1630f714a982379190b750b1e344aac343c
brainfreeze1994-cloud <brainfreeze1994@gmail.com>
2026-07-30 23:54:38 +0400

    Update Message.java

--- app/src/main/java/com/jarvis/ai/Message.java
+++ app/src/main/java/com/jarvis/ai/Message.java
@@ -4,12 +4,20 @@ public class Message {
     public static final int TYPE_USER   = 0;
     public static final int TYPE_JARVIS = 1;
     public static final int TYPE_TYPING = 2;
+    public static final int TYPE_IMAGE  = 3;
 
     public final int    type;
     public final String text;
+    public final String imageUri;
 
     public Message(int type, String text) {
-        this.type = type;
-        this.text = text;
+        this.type     = type;
+        this.text     = text;
+        this.imageUri = null;
     }
-}
+
+    public Message(int type, String text, String imageUri) {
+        this.type     = type;
+        this.text     = text;
+        this.imageUri = imageUri;
+    }
